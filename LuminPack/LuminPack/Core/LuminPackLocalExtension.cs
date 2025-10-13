7a45881259a29369d5c7af7f741e703129b7d7ab
Light <40874758+TheLight-233@users.noreply.github.com>
2025-10-13 17:43:33 +0800

    Update LuminPackLocalExtension.cs

--- LuminPack/LuminPack/Core/LuminPackLocalExtension.cs
+++ LuminPack/LuminPack/Core/LuminPackLocalExtension.cs
@@ -4,11 +4,13 @@ namespace LuminPack.Core;
 
 public static class LuminPackLocalExtension
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void WriteValue<T>(ref this LuminPackWriter writer, scoped in T value)
     {
         LuminPackParseProvider.Cache<T>.Parser!.Serialize(ref writer, ref Unsafe.AsRef(in value)!);
     }
     
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ReadValue<T>(ref this LuminPackReader reader, scoped ref T value)
     {
         LuminPackParseProvider.Cache<T>.Parser!.Deserialize(ref reader, ref value!);
