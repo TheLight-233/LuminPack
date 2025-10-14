838d15ea14d213684bdc8f587beffb988b3b6a1c
Light <40874758+TheLight-233@users.noreply.github.com>
2025-10-14 17:11:48 +0800

    Update UnmanagedFormatter.cs

--- LuminPackGenerator/Formatter/UnmanagedFormatter.cs
+++ LuminPackGenerator/Formatter/UnmanagedFormatter.cs
@@ -8,12 +8,12 @@ public static class UnmanagedFormatter
     public static void GenerateSerializeCode(LuminLocalFieldData data, StringBuilder sb)
     {
         sb.AppendLine("            Unsafe.WriteUnaligned(ref writer.GetCurrentSpanReference(), value);");
-        sb.AppendLine("            writer.Advance(Unsafe.SizeOf<T>());");
+        sb.AppendLine($"            writer.Advance(Unsafe.SizeOf<{data.TypeName}>());");
     }
     
     public static void GenerateDeserializeCode(LuminLocalFieldData data, StringBuilder sb)
     {
-        sb.AppendLine("            value = Unsafe.ReadUnaligned<T>(ref reader.GetCurrentSpanReference());");
-        sb.AppendLine("            reader.Advance(Unsafe.SizeOf<T>());");
+        sb.AppendLine($"            value = Unsafe.ReadUnaligned<{data.TypeName}>(ref reader.GetCurrentSpanReference());");
+        sb.AppendLine($"            reader.Advance(Unsafe.SizeOf<{data.TypeName}>());");
     }
 }
