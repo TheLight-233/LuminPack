a08d3a6eee6bcaf4b9bcf9589a0a54751c956d1d
Light <40874758+TheLight-233@users.noreply.github.com>
2025-10-16 16:27:11 +0800

    Update LuminConstructorData.cs

--- LuminPackGenerator/CodeInfo/LuminConstructorData.cs
+++ LuminPackGenerator/CodeInfo/LuminConstructorData.cs
@@ -1,8 +1,21 @@
+using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+
 namespace LuminPack.Code;
 
 public sealed class LuminConstructorData
 {
+    public List<ConstructorParameter> Parameters { get; set; } = new();
+    public bool IsMarkedWithAttribute { get; set; }
+    public Accessibility Accessibility { get; set; }
+    public int ParameterCount => Parameters.Count;
+}
+
+public sealed class ConstructorParameter
+{
     public string Name { get; set; }
     public string Type { get; set; }
-    public string Namespace { get; set; }
+    public string TypeNamespace { get; set; }
+    public bool MatchesField { get; set; }
+    public string MatchingFieldName { get; set; }
 }
