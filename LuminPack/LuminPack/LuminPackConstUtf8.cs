361bc06d6ca9025323b847ce6c29bac02b428240
Light <40874758+TheLight-233@users.noreply.github.com>
2026-01-17 17:33:06 +0800

    Add files via upload

---
+++ LuminPack/LuminPack/LuminPackConstUtf8.cs
@@ -0,0 +1,10 @@
+namespace LuminPack;
+
+public static class LuminPackConstUtf8
+{
+    // "$type" in UTF8 bytes: $ (36), t (116), y (121), p (112), e (101)
+    public static readonly byte[] TypeU8 = new byte[] { 36, 116, 121, 112, 101 };
+    
+    // "$value" in UTF8 bytes: $ (36), v (118), a (97), l (108), u (117), e (101)
+    public static readonly byte[] ValueU8 = new byte[] { 36, 118, 97, 108, 117, 101 };
+}
